45e927ffd2be5878ab60100c9789913dc3d970a6
Faheem-Ahmad911 <sheikhfahad5768@gmail.com>
2025-07-28 22:50:39 +0500

    Update modified files

--- circular_linedlist.cpp
+++ circular_linedlist.cpp
@@ -1,152 +1,49 @@
 #include <iostream>
 using namespace std;
-
-class Node {
-public:
+struct Node
+{
     int data;
-    Node* next;
-
-    Node(int val) {
+    Node *next;
+    Node(int val)
+    {
         data = val;
         next = nullptr;
     }
 };
-
-class CircularLinkedList {
-private:
-    Node* head;
-
-public:
-    CircularLinkedList() {
+struct circular_linedlist
+{
+    /* data */
+    Node *head;
+    circular_linedlist()
+    {
         head = nullptr;
     }
-
-    // Insert at the end
-    void insert(int val) {
-        Node* newNode = new Node(val);
-
-        if (head == nullptr) {
-            head = newNode;
-            head->next = head;  // Points to itself
-            return;
-        }
-
-        Node* temp = head;
-        while (temp->next != head) {
-            temp = temp->next;
-        }
-
-        temp->next = newNode;
-        newNode->next = head;
-    }
-
-    // Insert at the beginning
-    void insertAtBeginning(int val) {
-        Node* newNode = new Node(val);
-
-        if (head == nullptr) {
-            head = newNode;
+    void Insert_at_the_end(int v)
+    {
+        Node *newnode = new Node(v);
+        if (!head)
+        {
+            head = newnode;
             head->next = head;
-            return;
+            head = newnode;
         }
-
-        Node* temp = head;
-        while (temp->next != head) {
-            temp = temp->next;
-        }
-
-        temp->next = newNode;
-        newNode->next = head;
-        head = newNode;  // Update head
-    }
-
-    // Delete node by value
-    void deleteNode(int val) {
-        if (head == nullptr) {
-            cout << "List is empty.\n";
-            return;
-        }
-
-        Node* curr = head;
-        Node* prev = nullptr;
-
-        // Special case: deleting head
-        if (head->data == val) {
-            // Only one node
-            if (head->next == head) {
-                delete head;
-                head = nullptr;
-                return;
+        else{
+            Node * temp =head;
+            while(temp!=nullptr){
+                temp=temp->next;
             }
-
-            // Find last node to update its next
-            Node* last = head;
-            while (last->next != head) {
-                last = last->next;
-            }
-
-            last->next = head->next;
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-            return;
+            temp->next=newnode;
+            newnode->next=head;
+            head = newnode;
         }
-
-        // Deleting non-head node
-        do {
-            prev = curr;
-            curr = curr->next;
-
-            if (curr->data == val) {
-                prev->next = curr->next;
-                delete curr;
-                return;
-            }
-        } while (curr != head);
-
-        cout << "Value " << val << " not found in the list.\n";
     }
-
-    // Display the list
-    void display() {
-        if (head == nullptr) {
-            cout << "List is empty.\n";
-            return;
-        }
-
-        Node* temp = head;
-        do {
-            cout << temp->data << " ";
-            temp = temp->next;
-        } while (temp != head);
-        cout << endl;
-    }
-
-    // Search for an element
-    bool search(int val) {
-        if (head == nullptr) return false;
-
-        Node* temp = head;
-        do {
-            if (temp->data == val)
-                return true;
-            temp = temp->next;
-        } while (temp != head);
-
-        return false;
-    }
-
-    // Destructor to free memory
-    ~CircularLinkedList() {
-        if (head == nullptr) return;
-
-        Node* curr = head->next;
-        while (curr != head) {
-            Node* temp = curr;
-            curr = curr->next;
-            delete temp;
-        }
-        delete head;
-        head = nullptr;
+    void Delete_at_given_key(int index){
+        if (!head) return;
+        if (!head->next) 
     }
 };
+
+int main()
+{
+    return 0;
+}
--- prac.cpp
+++ prac.cpp
@@ -1,6 +1,30 @@
-#include<iostream>
-using namespace std;
-int main()
-{   cout<<7/2<<endl;
-      return 0;
+void List::merge(List& list1, List& list2) {
+    Node* t1 = list1.head;
+    Node* t2 = list2.head;
+
+    // Clear this list first
+    head = nullptr;
+    Node* tail = nullptr;
+
+    while (t1 != nullptr && t2 != nullptr) {
+        // Take node from list1
+        if (head == nullptr) {
+            head = t1;
+            tail = t1;
+        } else {
+            tail->next = t1;
+            tail = tail->next;
+        }
+        t1 = t1->next;
+
+        // Take node from list2
+        tail->next = t2;
+        tail = tail->next;
+        t2 = t2->next;
+    }
+
+    // Set ends and empty the other lists
+    tail->next = nullptr;
+    list1.head = nullptr;
+    list2.head = nullptr;
 }
