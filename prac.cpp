eeaf335eefef0868f5a2d1705d56edf41aad1034
Faheem-Ahmad911 <sheikhfahad5768@gmail.com>
2025-07-29 13:41:10 +0500

    Update modified files

--- prac.cpp
+++ prac.cpp
@@ -1,56 +1,70 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-struct Queue{
+struct CircularQueue
+{
+    int front, rear, size;
     int *arr;
-    int front,rear,size;
 
-    Queue(int s){
-        size =s;
-        front,rear=-1;
+    CircularQueue(int s)
+    {
+        size = s;
         arr = new int[s];
+        front = -1;
+        rear = -1;
     }
-    bool isEmpty(){
-        return rear == -1;
+    bool isEmpty()
+    {
+        return front == -1;
+    }
+    bool isFull()
+    {
+        return front == (rear + 1) % size;
+    }
+    void Enqueue(int val)
+    {
+        if (isFull())
+        {
+            cout << "Queue is full" << endl;
+            return;
+        }
+        if (isEmpty())
+        {
+            front = 0;
+            rear = 0;
+        }
+        else
+        {
+            rear = (rear + 1) % size;
+        }
+        arr[rear] = val;
+    }
+    void Dequeue(){
+        if (isEmpty()) {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        // Optionally print the dequeued value
+        // cout << arr[front] << endl;
+        if (front == rear)
+            front = rear = -1;
+        else
+            front = (front + 1) % size;
+    }
+    // void Display(){
+    //     if(!isEmpty()){
+    //         for (int i = front;i<rear;i++){
 
-    }
-    bool isfull(){
-        return front == rear;
-    }
-
-    void enqueue(int val){
-        if (isEmpty()){
-            front=0;
-        }
-        if (!isfull()){
-            arr[++rear]=val;
-        }
-        
-    }
-    void dequeue(){
-        if (!isEmpty()){
-            cout<<arr[front++]<<"Is dequeued"<<endl;
-
-        }
-    }
-    void top(){
-        if (!isEmpty()){
-            cout<<arr[front]<<" Front element "<<endl;
-        }
-    }
-    void Display(){
-        if (!isEmpty()){
-            cout<<arr[front]<<"  IS DISPLAYED"<<endl;
-        }
-    }
+    //             cout<<arr[front]<<endl;
+    //         }
+    //     }
+    // }
+    
 };
 int main()
-{   Queue q1(5);
-    q1.enqueue(12);
-    // q1.dequeue();
-    q1.enqueue(1);
-    q1.enqueue(2);
-    q1.top();
-    q1.Display();
-
-      return 0;
+{   CircularQueue q1(12);
+    q1.Enqueue(12);
+    q1.Enqueue(13);
+    q1.Dequeue();
+    // q1.Display();
+    return 0;
 }
